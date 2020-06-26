6ab774117cf4bd082ab4d2c56c9db677b133460b
Alana Bellucci <fastalana@gmail.com>
2020-06-26 13:44:06 -0600

    Working Javascript that scrolls to each section via a link.

--- js/app.js
+++ js/app.js
@@ -13,33 +13,60 @@
  * 
 */
 
-// Global Variables 
+// Global Variables
+
 const navigationElements = document.querySelectorAll('section')
 const navigationList = document.getElementById('navbar__list')
 
-// Build the navigation
+// Build a menu by iterating through the NavigationElements
+navigationElements.forEach(element => {
+  const navigationListElement = `<li class='menu__link ${element.className}' data-link=${element.id}><a href="#${element.id}">${element.dataset.nav}</li>`
+  navigationList.insertAdjacentHTML('beforeend', navigationListElement)
+})
 
-// navigationElements.forEach(el => {
-//     const navlistElement = `<li class='menu__link ${el.className}' data-link=${el.id}><a href="#${el.id}">${el.dataset.nav}</li>`
-//     navList.insertAdjacentHTML('beforeend', navlistElement)
-//   })
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
-
+// Scroll to the section by listenting to a click-event in the NavigationList
+navigationList.addEventListener('click', event => {
+    event.preventDefault()
+    const parent = event.target.hasAttribute('data-link')
+      ? event.target
+      : event.target.parentElement
+    const elementToScrollTo = document.getElementById(parent.dataset.link)
+    elementToScrollTo.scrollIntoView({block: 'end', behavior: 'smooth'})
+  })
+  
+// Set section and nav link as active using IntersectionObserver
+const callback = entries => {
+    entries.forEach(entry => {
+      const navigationListElement = document.querySelector(
+        `.menu__link[data-link='${entry.target.id}']`,
+      )
+      const section = document.getElementById(entry.target.id)
+  
+      if (entry && entry.isIntersecting) {
+        navigationListElement.classList.add('active')
+        section.classList.add('active')
+      } else {
+        if (navigationListElement.classList.contains('active')) {
+          navigationListElement.classList.remove('active')
+        }
+  
+        if (section.classList.contains('active')) {
+          section.classList.remove('active')
+        }
+      }
+    })
+  }
+  
+// Options for the observer. Used in the IntersectionObserver function.
+// const options = {
+//     root: null,
+//     rootMargin: '0px',
+//     threshold: 0.6,
+//   }
+  
+// Setting an observer with options and a callback which checks if the navigation element should be active
+// const observer = new IntersectionObserver(callback, options)
+const observer = new IntersectionObserver(callback)
+navigationElements.forEach(element => {
+    observer.observe(document.getElementById(element.id))
+  })
