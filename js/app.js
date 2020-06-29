8092daf84608cea73f10623bee4aecdc1a51f28c
Alana Bellucci <fastalana@gmail.com>
2020-06-29 11:49:01 -0600

    Creating the navigation menu from javascript. Understanding the implications of 'afterbegin, afterend, beforebegin, and before end for innerAdjacentHTML.

--- js/app.js
+++ js/app.js
@@ -12,3 +12,15 @@
  * JS Standard: ESlint
  * 
 */
+
+// Global Variables
+const navigationElements = document.querySelectorAll('section');  //captures all items with the <section> tag
+const navigationList = document.getElementById('navbar__list');  //captures the id of the unordered list which will be populated with javascript below.
+
+navigationElements.forEach(element => {
+  const navigationListElement = `<li class='menu__link' data-link=${element.id}><a href="#${element.id}">${element.dataset.nav}</li>`
+  // navigationList.insertAdjacentHTML('afterend', navigationListElement)  // ascending, aligned left
+  // navigationList.insertAdjacentHTML('beforebegin', navigationListElement) // descending, aligned left
+  navigationList.insertAdjacentHTML('beforeend', navigationListElement) // ascending, aligned right
+  // navigationList.insertAdjacentHTML('afterbegin', navigationListElement) // descending, aligned left
+})
