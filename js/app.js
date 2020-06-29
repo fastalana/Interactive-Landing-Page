b7763c757fb1a3332d2d36d45f4c2b3c2066153b
Alana Bellucci <fastalana@gmail.com>
2020-06-29 14:40:59 -0600

    Setting up IntersectionObserver pattern, from MDN documentation, https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API

--- js/app.js
+++ js/app.js
@@ -23,3 +23,32 @@ navigationElements.forEach(element => {
   const navigationListElement = `<li class='menu__link' data-link=${element.id}><a href="#${element.id}">${element.dataset.nav}</li>`
   navigationList.insertAdjacentHTML('beforeend', navigationListElement) // 'beforeend': Just inside the element (li), after its last child.
 })
+
+// Show what section from the navigation has been selected 
+// and/or is being viewed while scrolling
+let callback = (entries, observer) => {
+  entries.forEach(entry => {
+    // Each entry describes an intersection change for one observed
+    // target element:
+    //   entry.boundingClientRect
+    //   entry.intersectionRatio
+    //   entry.intersectionRect
+    //   entry.isIntersecting
+    //   entry.rootBounds
+    //   entry.target
+    //   entry.time
+  });
+};
+
+// Options object that will be passed into the IntersectionObserver() constructor
+// Controls the when the callback is invoked
+let options = {
+  root: null, // Element that is used as the viewport for checking visibility of the target; null = defaults to the browser default
+  rootMargin: '0px', // Margin around the root
+  threshold: 1.0  //  The percentage of the target's visibility the observer's callback should be executed
+}
+
+let observer = new IntersectionObserver(callback, options);
+let target = document.querySelector('#listItem');
+observer.observe(target);
+
