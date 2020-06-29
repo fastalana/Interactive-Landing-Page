0e906dbffe24357d6f0f391bf820c9b9722a801d
Alana Bellucci <fastalana@gmail.com>
2020-06-29 14:24:45 -0600

    Starting development for updating CSS for active sections.

--- js/app.js
+++ js/app.js
@@ -13,10 +13,12 @@
  * 
 */
 
-// Global Variables
-const navigationElements = document.querySelectorAll('section');  //captures all items with the <section> tag
-const navigationList = document.getElementById('navbar__list');  //captures the id of the unordered list which will be populated with javascript below.
+// Global variables
+const navigationElements = document.querySelectorAll('section');  // captures all items with the <section> tag
+const navigationList = document.getElementById('navbar__list');  // captures the id of the unordered list which will be populated with javascript below.
+const activeSection = document.querySelectorAll('.active'); // to start nothing is active, sections get set to active in the javascript below.
 
+// Build navigation list
 navigationElements.forEach(element => {
   const navigationListElement = `<li class='menu__link' data-link=${element.id}><a href="#${element.id}">${element.dataset.nav}</li>`
   navigationList.insertAdjacentHTML('beforeend', navigationListElement) // 'beforeend': Just inside the element (li), after its last child.
